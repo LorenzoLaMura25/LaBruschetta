5e53fd05d7ce678e93383f7a3dd59381a6cae84e
LorenzoLaMuraPro <lori1197.lichess@gmail.com>
2024-06-24 16:00:49 +0200

    edit mobile

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css'; // Importa il file CSS
 
 const Navbar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-brand">
-        MyBrand
+        La Bruschetta
       </div>
-      <ul className="navbar-links">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/menu">Menu</Link></li>
-        <li><Link to="/prenota">Prenota</Link></li>
-      </ul>
+      <div className={`navbar-menu ${isMobileMenuOpen ? 'is-active' : ''}`}>
+        <ul className="navbar-links">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/menu">Menu</Link></li>
+          <li><Link to="/prenota">Prenota</Link></li>
+        </ul>
+      </div>
+      <button className="navbar-burger" onClick={toggleMobileMenu}>
+        &#9776;
+      </button>
     </nav>
   );
 }
