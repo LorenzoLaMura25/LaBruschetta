ed155ae640f91950d2b8805f00956ae5c29dd1b4
LorenzoLaMuraPro <lori1197.lichess@gmail.com>
2024-07-10 09:51:00 +0200

    edit navbar resposnsive

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -18,33 +18,44 @@ const Navbar = () => {
     i18n.changeLanguage(newLanguage.toLowerCase());
   };
 
+  const closeMobileMenu = () => {
+    setIsMobileMenuOpen(false);
+  };
+
   return (
-    <nav className="navbar">
-      <div className="navbar-brand">
-        <Link to="/">
-          <img src="./logo.png" alt="Logo" />
-        </Link>
-      </div>
-      <div className={`navbar-menu ${isMobileMenuOpen ? "is-active" : ""}`}>
-        <ul className="navbar-links">
-          <li>
-            <Link to="/">{i18n.t("Home")}</Link>
-          </li>
-          <li>
-            <Link to="/menu">{i18n.t("Menu")}</Link>
-          </li>
-          <li>
-            <Link to="/prenota">{i18n.t("Prenota")}</Link>
-          </li>
-          <li>
-            <Link to="/azienda">{i18n.t("Collabora con Noi")}</Link>
-          </li>
-        </ul>
-        <button className="language-switch" onClick={switchLanguage}>
-          {language}
+    <>
+      <nav className="navbar">
+        <div className="navbar-brand">
+          <Link to="/" onClick={closeMobileMenu}>
+            <img src="./logo.png" alt="Logo" />
+          </Link>
+        </div>
+        <button className="hamburger" onClick={toggleMobileMenu}>
+          &#9776;
         </button>
-      </div>
-    </nav>
+        <div className={`navbar-menu ${isMobileMenuOpen ? "is-active" : ""}`}>
+          <ul className="navbar-links">
+            <li>
+              <Link to="/" onClick={closeMobileMenu}>{i18n.t("Home")}</Link>
+            </li>
+            <li>
+              <Link to="/menu" onClick={closeMobileMenu}>{i18n.t("Menu")}</Link>
+            </li>
+            <li>
+              <Link to="/prenota" onClick={closeMobileMenu}>{i18n.t("Prenota")}</Link>
+            </li>
+            <li>
+              <Link to="/azienda" onClick={closeMobileMenu}>{i18n.t("Collabora con Noi")}</Link>
+            </li>
+          </ul>
+          <button className="language-switch" onClick={switchLanguage}>
+            {language}
+          </button>
+        </div>
+      </nav>
+      {isMobileMenuOpen && <div className="overlay" onClick={toggleMobileMenu}></div>}
+      <div className="content-padding"></div> {/* Aggiungi questo div per il padding */}
+    </>
   );
 };
 
