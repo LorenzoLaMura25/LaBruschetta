f13230e7874cd5124a5bcf6cf02d306a0dcf5c04
LorenzoLaMuraPro <lori1197.lichess@gmail.com>
2024-07-11 09:45:54 +0200

    edit translation

--- src/Pages/Azienda/Azienda.jsx
+++ src/Pages/Azienda/Azienda.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
+import { useTranslation } from 'react-i18next';
 import "./Azienda.css";
 
 const FormPrenotazione = () => {
+  const { t } = useTranslation();
+
   const [formData, setFormData] = useState({
     data: "",
     orario: "",
     numeroPersone: 1,
     nome: "",
-    cognome: "",
     email: "",
     telefono: "",
   });
@@ -27,85 +29,82 @@ const FormPrenotazione = () => {
       orario: "",
       numeroPersone: 1,
       nome: "",
-      cognome: "",
       email: "",
       telefono: "",
     });
   };
 
   return (
-
     <>
-    <div className="background-diagonal-azienda"></div>
-    <div className="azienda-form-container">
-      <h2>Form di Prenotazione Azienda</h2>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="data">Scegli la data:</label>
-        <input
-          type="date"
-          id="data"
-          name="data"
-          value={formData.data}
-          onChange={handleChange}
-          required
-        />
+      <div className="background-diagonal-azienda"></div>
+      <div className="azienda-form-container">
+        <h2>{t('Form di Prenotazione Azienda')}</h2>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="data">{t('Scegli la data')}:</label>
+          <input
+            type="date"
+            id="data"
+            name="data"
+            value={formData.data}
+            onChange={handleChange}
+            required
+          />
 
-        <label htmlFor="orario">Scegli l'orario:</label>
-        <input
-          type="time"
-          id="orario"
-          name="orario"
-          value={formData.orario}
-          onChange={handleChange}
-          required
-        />
+          <label htmlFor="orario">{t('Scegli l\'orario')}:</label>
+          <input
+            type="time"
+            id="orario"
+            name="orario"
+            value={formData.orario}
+            onChange={handleChange}
+            required
+          />
 
-        <label htmlFor="numeroPersone">Numero di Dipendenti:</label>
-        <input
-          type="number"
-          id="numeroPersone"
-          name="numeroPersone"
-          min="1"
-          value={formData.numeroPersone}
-          onChange={handleChange}
-          required
-        />
+          <label htmlFor="numeroPersone">{t('Numero di Dipendenti')}:</label>
+          <input
+            type="number"
+            id="numeroPersone"
+            name="numeroPersone"
+            min="1"
+            value={formData.numeroPersone}
+            onChange={handleChange}
+            required
+          />
 
-        <label htmlFor="nome">Nome Azienda:</label>
-        <input
-          type="text"
-          id="nome"
-          name="nome"
-          value={formData.nome}
-          onChange={handleChange}
-          required
-        />
+          <label htmlFor="nome">{t('Nome Azienda')}:</label>
+          <input
+            type="text"
+            id="nome"
+            name="nome"
+            value={formData.nome}
+            onChange={handleChange}
+            required
+          />
 
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          required
-        />
+          <label htmlFor="email">{t('Email')}:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
 
-        <label htmlFor="telefono">Telefono:</label>
-        <input
-          type="tel"
-          id="telefono"
-          name="telefono"
-          value={formData.telefono}
-          onChange={handleChange}
-          required
-        />
+          <label htmlFor="telefono">{t('Telefono')}:</label>
+          <input
+            type="tel"
+            id="telefono"
+            name="telefono"
+            value={formData.telefono}
+            onChange={handleChange}
+            required
+          />
 
-        <button type="submit">Contattaci</button>
-      </form>
-    </div>
+          <button type="submit">{t('Contattaci')}</button>
+        </form>
+      </div>
     </>
-    
   );
 };
 
